5001a11d7343fd67bc78e3c1889063f043193660
qingguanfeng <619595145@qq.com>
2016-10-06 16:07:54 +0800

    覃上传的

---
+++ bookshop_web/src/test/java/TestOne.java
@@ -0,0 +1,24 @@
+import com.fc.platform.commons.page.Page;
+/*import com.qinguangfeng.Start;
+import com.qinguangfeng.dao.entity.Topic;
+import com.qinguangfeng.services.LogicTopic;*/
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+/*@SpringApplicationConfiguration(value = Start.class)*/
+public class TestOne {
+
+
+    @Test
+    public void testFindAll() {
+
+    }
+
+
+}
