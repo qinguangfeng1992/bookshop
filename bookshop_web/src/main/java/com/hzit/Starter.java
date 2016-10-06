895fc5c9c00e003acea8107b5e014b05f2290e02
xiesansan3 <1141214047@qq.com>
2016-10-06 16:29:45 +0800

    谢

--- bookshop_web/src/main/java/com/hzit/Starter.java
+++ bookshop_web/src/main/java/com/hzit/Starter.java
@@ -1,7 +1,18 @@
 package com.hzit;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
 /**
  * Created by Administrator on 2016/10/6.
  */
-public class Starter {
+@SpringBootApplication
+public class Starter extends SpringBootServletInitializer{
+    public Starter(){
+        System.out.println("启动SpringApplication");
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Starter.class, args);
+    }
 }
