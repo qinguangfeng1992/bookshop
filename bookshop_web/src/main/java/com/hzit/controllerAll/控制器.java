15d7ccac90133e7ffea513a12e774368c6edbe8a
ab1324ab <435821607@qq.com>
2016-10-06 16:02:35 +0800

    哈哈哈

---
+++ bookshop_web/src/main/java/com/hzit/controllerAll/控制器.java
@@ -0,0 +1,7 @@
+package com.hzit.controllerAll;
+
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+public class 控制器 {
+}
---
+++ bookshop_web/src/main/java/com/hzit/serviceAll/serviceImpl/业务器实现类.java
@@ -0,0 +1,7 @@
+package com.hzit.serviceAll.serviceImpl;
+
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+public class 业务器实现类 {
+}
