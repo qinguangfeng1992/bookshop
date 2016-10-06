19a9228a095ff07f967b5d9012d40e9c75089944
ab1324ab <435821607@qq.com>
2016-10-06 15:58:34 +0800

    哈哈哈

---
+++ bookshop_web/src/test/TestHou.java
@@ -0,0 +1,14 @@
+import com.hzit.Starter;
+import org.hamcrest.beans.SamePropertyValuesAs;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(value = Starter.class)
+public class TestHou {
+
+}
