b5d12a0864e426c050c5507d49b190a8c0bd5f33
xiesansan3 <1141214047@qq.com>
2016-10-07 13:49:03 +0800

    规范命名

---
+++ bookshop_web/src/main/java/com/hzit/controller/BookContro.java
@@ -0,0 +1,43 @@
+package com.hzit.controller;
+
+import com.fc.platform.commons.page.Page;
+import com.hzit.dao.entity.Book;
+import com.hzit.service.BookQin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+@Controller
+public class BookContro {
+    @Autowired
+    private BookQin bookImpl;
+
+/*测试一下能不能 把JSON字符串 发送到页面上的 查询全部的方法*/
+    @RequestMapping("/bookall")
+    @ResponseBody
+    public Object getBookList() {
+        return bookImpl.findBookAll();
+
+    }
+
+    //首页
+    @RequestMapping("/bookindex")
+    public String getBookIndex(@RequestParam(name = "page", defaultValue = "0") Integer page, ModelMap modelMap) {
+        if (page < 0) page = 0;
+
+
+        Page<Book> list = bookImpl.findPage(page,3);
+        modelMap.put("list",list);
+        modelMap.put("currpage", page);
+        return "index";
+
+    }
+
+}
---
+++ bookshop_web/src/main/java/com/hzit/controller/HouOrder.java
@@ -0,0 +1,47 @@
+package com.hzit.controller;
+
+import com.hzit.dao.entity.Book;
+import com.hzit.dao.entity.Orderdetail;
+import com.hzit.service.OrderDelHou;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+@Controller
+@RequestMapping("/hou")
+public class HouOrder {
+
+    @Autowired
+    OrderDelHou orderAll;
+    @RequestMapping("/orderlist")
+    public String all(@RequestParam(name = "bookid",defaultValue = "1")String bookid, ModelMap modelMap,HttpServletRequest request) {
+        String userid = "1";//(String) request.getSession().getAttribute("userid");
+        String boo = bookid;
+        if (!boo.equals(bookid)){
+            //添加订单
+            orderAll.Allorder(bookid, userid);
+        }
+        //查询当前用户的图书订单
+        List<Orderdetail> list=orderAll.Aorder(userid);
+        //创建图书集
+        List<Book> book=new ArrayList<Book>();
+        //循环保存当前的用户 所有图书
+        for (int i=0;i<list.size();i++) {
+            Orderdetail o=list.get(i);
+            Book b=orderAll.bookA(o.getBookid());
+            book.add(i,b);
+        }
+        //保存带走
+        modelMap.put("book",book);
+        return "shopping";
+    }
+}
