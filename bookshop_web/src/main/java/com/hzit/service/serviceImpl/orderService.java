d05f4c34ca380c0b6bbb0bb2fe1f559ddfa47346
xiesansan3 <1141214047@qq.com>
2016-10-08 11:16:53 +0800

    三

--- bookshop_web/src/main/java/com/hzit/service/serviceImpl/orderService.java
+++ bookshop_web/src/main/java/com/hzit/service/serviceImpl/orderService.java
@@ -18,7 +18,6 @@ public class OrderService implements OrderXie {
 
     @Override
     public List<Order> findOrderAll() {
-        System.out.println("");
         return orderMapper.searchOrderByParams(null);
     }
 }
