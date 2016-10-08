46cc3ccdc6f57c238c6df4a5e6e3e46e60094193
xiesansan3 <1141214047@qq.com>
2016-10-08 09:03:44 +0800

    三

---
+++ bookshop_web/src/main/java/com/hzit/controller/OrderController.java
@@ -0,0 +1,36 @@
+package com.hzit.controller;
+
+import com.hzit.dao.entity.Order;
+import com.hzit.service.serviceImpl.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/10/7.
+ */
+@Controller
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @RequestMapping("/toorderlist")
+     public String findOrderList(ModelMap modelMap){
+        List<Order> list=orderService.findOrderAll();
+        modelMap.put("olist",list);
+        return "orderlist";
+    }
+    @RequestMapping("/toorderlist")
+    @ResponseBody
+    public String orderList(ModelMap modelMap){
+        List<Order> list=orderService.findOrderAll();
+        modelMap.put("olist",list);
+        return "orderlist";
+    }
+}
